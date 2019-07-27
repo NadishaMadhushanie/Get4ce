5d4150d7c703e1d192d94ab27205a56f888d9b99
lakshikahasi <42285794+lakshikahasi@users.noreply.github.com>
2019-07-27 14:29:41 +0530

    Update Get4ce.java

--- Get4ce.java
+++ Get4ce.java
@@ -1,119 +1,72 @@
+/**
+ * Copyright (c) 2001-2019 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * https://robocode.sourceforge.io/license/epl-v10.html
+ */
 
+package a;
 
-/*
-import robocode.HitRobotEvent;
-import robocode.Robot;
-import robocode.ScannedRobotEvent;
-*/
 import robocode.*;
+
 import java.awt.*;
-import static robocode.util.Utils.normalRelativeAngleDegrees;
 
-public class Get4ce extends AlphaBot {
 
-	boolean peek; // Don't turn if there's a robot there
-	double moveAmount; // How much to move
+/**
+ * SpinBot - a sample robot by Mathew Nelson.
+ * <p>
+ * Moves in a circle, firing hard when an enemy is detected.
+ *
+ * @author Mathew A. Nelson (original)
+ * @author Flemming N. Larsen (contributor)
+ */
+public class Get4ce extends BravoBot {
 
 	/**
-	 * run: Move around the walls
+	 * SpinBot's run method - Circle
 	 */
 	public void run() {
 		// Set colors
 		setBodyColor(Color.blue);
 		setGunColor(Color.black);
 		setRadarColor(Color.red);
-		setBulletColor(Color.cyan);
-		setScanColor(Color.cyan);
+		setScanColor(Color.yellow);
 
-		// Initialize moveAmount to the maximum possible for this battlefield.
-		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
-		// Initialize peek to false
-		peek = false;
-
-		// turnLeft to face a wall.
-		// getHeading() % 90 means the remainder of
-		// getHeading() divided by 90.
-		turnLeft(getHeading() % 90);
-		ahead(moveAmount);
-		// Turn the gun to turn right 90 degrees.
-		peek = true;
-		turnGunRight(90);
-		turnRight(90);
-
-		while (true) {
-			// Look before we turn when ahead() completes.
-			peek = true;
-			// Move up the wall
-			ahead(moveAmount);
-			// Don't look now
-			peek = false;
-			// Turn to the next wall
+		// Loop forever
+	ahead(40000);
+	
 			turnRight(90);
-		}
+			
+			//waitFor(new TurnCompleteCondition(this));
+			
+			turnLeft(180);
+		
+			//waitFor(new TurnCompleteCondition(this));
+		
+			turnRight(180);
+			
+			//waitFor(new TurnCompleteCondition(this));
+		
 	}
 
 	/**
-	 * onHitRobot:  Move away a bit.
+	 * onScannedRobot: Fire hard!
 	 */
-/*	public void onHitRobot(HitRobotEvent e) {
-		// If he's in front of us, set back up a bit.
-		if (e.getBearing() > -90 && e.getBearing() < 90) {
-			back(100);
-		} // else he's in back of us, so set ahead a bit.
-		else {
-			ahead(100);
-		}
-	}*/
-	
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Calculate exact location of the robot
-		double absoluteBearing = getHeading() + e.getBearing();
-		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
-
-		// If it's close enough, fire!
-		if (Math.abs(bearingFromGun) <= 3) {
-			turnGunRight(bearingFromGun);
-			// We check gun heat here, because calling fire()
-			// uses a turn, which could cause us to lose track
-			// of the other robot.
-			if (getGunHeat() == 0) {
-				fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
-			}
-		} // otherwise just set the gun to turn.
-		// Note:  This will have no effect until we call scan()
-		else {
-			turnGunRight(bearingFromGun);
-		}
-		// Generates another scan event if we see a robot.
-		// We only need to call this if the gun (and therefore radar)
-		// are not turning.  Otherwise, scan is called automatically.
-		if (bearingFromGun == 0) {
-			scan();
-		}
+		fire(3);
 	}
-	
+
+	/**
+	 * onHitRobot:  If it's our fault, we'll stop turning and moving,
+	 * so we need to turn again to keep spinning.
+	 */
 	public void onHitRobot(HitRobotEvent e) {
-		// If he's in front of us, set back up a bit.
-		if (e.getBearing() > -90 && e.getBearing() < 90) {
-			back(100);
-		} // else he's in back of us, so set ahead a bit.
-		else {
-			ahead(100);
-		}
-	
-		// Determine a shot that won't kill the robot...
-		// We want to ram him instead for bonus points
-		if (e.getEnergy() > 16) {
+		if (e.getBearing() > -10 && e.getBearing() < 10) {
 			fire(3);
-		} else if (e.getEnergy() > 10) {
-			fire(2);
-		} else if (e.getEnergy() > 4) {
-			fire(1);
-		} else if (e.getEnergy() > 2) {
-			fire(1);
-		} else if (e.getEnergy() > .4) {
-			fire(1);
 		}
-		ahead(40); // Ram him again!
+		if (e.isMyFault()) {
+			turnRight(10);
+		}
 	}
 }
